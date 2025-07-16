e1fa44de37a8afb2e455e1d508846cbf7af319af
Shreyash-Bankar <shreyashbankar10@gmail.com>
2025-07-16 22:38:00 +0530

    Reversing priority in priority queue

--- 10. Heaps/Priority_Queue.java
+++ 10. Heaps/Priority_Queue.java
@@ -1,4 +1,5 @@
 import java.util.PriorityQueue;
+import java.util.Comparator;
 public class Priority_Queue{
     public static void main(String args[]){
         PriorityQueue <Integer> pq = new PriorityQueue<>();
@@ -10,5 +11,16 @@ public class Priority_Queue{
             System.out.print(pq.peek()+" ");
             pq.remove();
         }
+        System.out.println();
+        PriorityQueue <Integer> pq2 = new PriorityQueue<>(Comparator.reverseOrder());
+        pq2.add(6);       
+        pq2.add(3);       
+        pq2.add(5);       
+        pq2.add(1);       
+        while(!pq2.isEmpty()){
+            System.out.print(pq2.peek()+" ");
+            pq2.remove();
+        }
+        System.out.println();
     }
 }
