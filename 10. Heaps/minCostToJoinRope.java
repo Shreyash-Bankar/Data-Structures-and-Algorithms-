cf01fcd1d2a4c1cc7e24224c598243839cab6299
Shreyash-Bankar <shreyashbankar10@gmail.com>
2025-07-22 09:11:36 +0530

    Minimum cost required to join rope is added

---
+++ 10. Heaps/minCostToJoinRope.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class minCostToJoinRope{
+    
+    public static void main(String args[]){
+        PriorityQueue <Integer> pq = new PriorityQueue<>();
+        pq.add(4); 
+        pq.add(3); 
+        pq.add(2); 
+        pq.add(6); 
+        int cost = 0;
+        while(pq.size()!=1){
+            int first = pq.remove(); 
+            int second = pq.remove(); 
+            int sum = first + second;
+            cost = cost + sum;
+            pq.add(sum);
+        }
+        System.out.print(cost);
+    }
+}
